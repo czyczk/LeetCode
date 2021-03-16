8edd8429f974bc94d397d3cf3573a533efd321ba
Zenas Chen <czyczk@qq.com>
2021-03-16 21:58:13 +0800

    59 C#

---
+++ csharp/lc-59/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace lc_59
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var s = new Solution();
+            // Expecting [[1, 2, 3], [8, 9, 4], [7, 6, 5]]
+            PrintMatrix(s.GenerateMatrix(3));
+        }
+
+        private static void PrintMatrix(int[][] matrix)
+        {
+            Console.Write("[");
+            var isFirst = true;
+            foreach (var line in matrix)
+            {
+                if (!isFirst)
+                {
+                    Console.Write(", ");
+                }
+                isFirst = false;
+
+                Console.Write("[");
+                var isFirstInLine = true;
+                foreach (var num in line)
+                {
+                    if (!isFirstInLine)
+                    {
+                        Console.Write(", ");
+                    }
+                    isFirstInLine = false;
+
+                    Console.Write(num);
+                }
+
+                Console.Write("]");
+            }
+
+            Console.WriteLine("]");
+        }
+    }
+}
---
+++ csharp/lc-59/Solution.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace lc_59
+{
+    public class Solution
+    {
+        public int[][] GenerateMatrix(int n)
+        {
+            // 0: right, 1: down, 2: left, 3: up
+            var curDirection = 0;
+            var totalSteps = new List<int> { n, n - 1, n - 1, n - 2 };
+            var remainingSteps = new List<int>(totalSteps);
+            var ret = new int[n][];
+            Enumerable.Range(0, n).ToList().ForEach(i => ret[i] = new int[n]);
+
+            remainingSteps[0]--;
+            ret[0][0] = 1;
+            var cnt = 1;
+            var i = 0;
+            var j = 0;
+
+            while (cnt < n * n)
+            {
+                if (remainingSteps[curDirection] > 0)
+                {
+                    remainingSteps[curDirection]--;
+                    // Update the next coordinate (position) in the current direction
+                    switch (curDirection)
+                    {
+                        case 0:
+                            j++;
+                            break;
+                        case 1:
+                            i++;
+                            break;
+                        case 2:
+                            j--;
+                            break;
+                        case 3:
+                            i--;
+                            break;
+                        default:
+                            throw new Exception();
+                    }
+                    // Set the matrix in the position with the desired number and update the count
+                    cnt++;
+                    ret[i][j] = cnt;
+                }
+                else
+                {
+                    // Each time the remaining steps of a direction are exausted, sub
+                    // total_steps of the current direction by 2.
+                    totalSteps[curDirection] -= 2;
+                    // Reset the remaining_steps of the current direction.
+                    remainingSteps[curDirection] = totalSteps[curDirection];
+                    // Change the direction clockwise.
+                    curDirection = (curDirection + 1) % 4;
+                }
+            }
+
+            return ret;
+        }
+    }
+}
