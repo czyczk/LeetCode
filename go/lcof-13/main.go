75c31c53313f8b8196869b54f2bb03be274dac71
Zenas Chen <czyczk@qq.com>
2021-04-16 16:54:48 +0800

    lcof-13

--- go/lcof-13/main.go
+++ go/lcof-13/main.go
@@ -4,9 +4,17 @@ import "fmt"
 
 func main() {
 	m1, n1, k1 := 2, 3, 1
+	m2, n2, k2 := 3, 1, 0
+	m3, n3, k3 := 4, 11, 13
 
 	// Expecting 3
 	fmt.Println(movingCount(m1, n1, k1))
+
+	// Expecting 1
+	fmt.Println(movingCount(m2, n2, k2))
+
+	// Expecting 1
+	fmt.Println(movingCount(m3, n3, k3))
 }
 
 type Status int
@@ -56,65 +64,26 @@ func movingCount(m, n, k int) int {
 		}
 	}
 
-	var stack [][]pair
-	queue := []pair{{0, 0}}
-	stack = append(stack, queue)
-	var curTrace []pair
-	maxSteps := 0
+	steps := make([][]int, m)
+	for i := 0; i < m; i++ {
+		steps[i] = make([]int, n)
+	}
 
-	for len(stack) != 0 {
-		queue := stack[len(stack)-1]
-		if len(queue) == 0 {
-			if len(curTrace) == 0 {
-				return maxSteps
+	for i := 0; i < m; i++ {
+		for j := 0; j < n; j++ {
+			if j-1 >= 0 {
+				steps[i][j] = steps[i][j-1]
+			} else if i-1 >= 0 {
+				steps[i][j] = steps[i-1][j] + 1
+			} else {
+				steps[i][j] = 0
 			}
 
-			idxRec := curTrace[len(curTrace)-1]
-			board[idxRec.i][idxRec.j] = Pending
-			curTrace = curTrace[:len(curTrace)-1]
-
-			stack = stack[:len(stack)-1]
-
-			continue
+			if board[i][j] == Pending {
+				steps[i][j]++
+			}
 		}
-
-		idx := queue[0]
-		stack[len(stack)-1] = queue[1:]
-		curTrace = append(curTrace, idx)
-		maxSteps = max(maxSteps, len(curTrace))
-		if maxSteps == m*n {
-			return maxSteps
-		}
-
-		board[idx.i][idx.j] = Visited
-
-		var adjCandidates []pair
-		if idx.j-1 >= 0 && board[idx.i][idx.j-1] == Pending {
-			adjCandidates = append(adjCandidates, pair{idx.i, idx.j - 1})
-		}
-		if idx.i-1 >= 0 && board[idx.i-1][idx.j] == Pending {
-			adjCandidates = append(adjCandidates, pair{idx.i - 1, idx.j})
-		}
-		if idx.j+1 < n && board[idx.i][idx.j+1] == Pending {
-			adjCandidates = append(adjCandidates, pair{idx.i, idx.j + 1})
-		}
-		if idx.i+1 < m && board[idx.i+1][idx.j] == Pending {
-			adjCandidates = append(adjCandidates, pair{idx.i + 1, idx.j})
-		}
-
-		stack = append(stack, adjCandidates)
 	}
 
-	return maxSteps
-}
-
-type pair struct {
-	i, j int
-}
-
-func max(x, y int) int {
-	if x > y {
-		return x
-	}
-	return y
+	return steps[m-1][n-1]
 }
